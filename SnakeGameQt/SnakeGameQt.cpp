d617415364f7d796f497914b7940505d43126461
LSERY <leo.sery@ynov.com>
2022-02-09 17:30:41 +0100

    add main window && snake background grid

--- SnakeGameQt/SnakeGameQt.cpp
+++ SnakeGameQt/SnakeGameQt.cpp
@@ -3,10 +3,32 @@
 SnakeGameQt::SnakeGameQt(QWidget *parent)
 	: QWidget(parent)
 {
-	
+	setFixedSize(680, 680);
+	m_GridLayout = new QGridLayout(this);
+	InitGrid();
+
+	setLayout(m_GridLayout);
 }
 
 void SnakeGameQt::InitGrid()
 {
+	m_GridLayout->setHorizontalSpacing(0);
+	m_GridLayout->setVerticalSpacing(0);
 
+	for (size_t i = 1; i < 18; i++)
+	{
+		m_GridLayout->setColumnMinimumWidth(i, 40);
+		for (size_t j = 1; j < 18; j++)
+		{
+			m_GridLayout->setColumnMinimumWidth(j, 40);
+			QWidget* w = new QWidget(this);
+
+			if ((i + j) % 2 == 0)
+				w->setStyleSheet("background-color: #A7D948");
+			else
+				w->setStyleSheet("background-color: #8ECC39");
+			
+			m_GridLayout->addWidget(w, j, i);
+		}
+	}
 }
--- SnakeGameQt/SnakeGameQt.h
+++ SnakeGameQt/SnakeGameQt.h
@@ -1,8 +1,15 @@
 #pragma once
 
 #include <QtWidgets/QMainWindow>
+#include <QGridLayout>
 
 class SnakeGameQt : public QWidget
 {
-   
+    Q_OBJECT
+
+public:
+    SnakeGameQt(QWidget *parent = Q_NULLPTR);
+private: 
+    void InitGrid();
+    QGridLayout* m_GridLayout;
 };
